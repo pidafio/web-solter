eb0afb96196b1e81a091edb371d9e028a045fc50
pidafio <pidafio@gmail.com>
2022-11-29 10:40:03 -0300

    antes de resposive 29/11/22

--- js/agregarGrilla.js
+++ js/agregarGrilla.js
@@ -1,27 +1,37 @@
- 
- 
- 
- 
- const grilla={
-  id:"id",
-  img:"img",
-  descripcion:"descripcion",
-  precio: "precio",
 
- }
+function articuloNuevo(){
+  const xhttp=  new XMLHttpRequest();
+  xhttp.open('GET','../JSON/grillaProductos.json', true);
+  xhttp.send();
+  xhttp.onreadystatechange= function(){
+    if(this.readyState==4 && this.status==200){
+       const datosProductos= JSON.parse(this.responseText);
+       for(var item of datosProductos){
+        console.log(datosProductos)
+    const crearGrilla= document.querySelector(".contenedor__grillas");
+    const plantilla=  
+           `<div class="grilla__Procuto">
+                <div class="img__paraGrilla" ><img class="imgGrilla" src="${item.pimg}"></div>
+                <div class="grilla__descripcion"> ${item.pnombre} <p class="texto__grilla-descripcion"> 
+                 ${item.pdescripcion}</p></div>
+                <h2 class="grilla__precio">Precio ${item.pprecio}</h2>
+                <button  id="btn" class="grilla__btnComprar">COMPRAR</button>
+            </div> `;
+    
+    crearGrilla.insertAdjacentHTML('afterend',plantilla);
+    console.log(crearGrilla) ;
 
-function articuloNuevo (img,descripcion,precio){
+    const btnComprar= document.querySelector("#btn");
+   
+    btnComprar.addEventListener("click",(e)=>{
+         e=window.location.href ="../html/compra-producto.html";
+       });
+      }
+    
 
-const montajeGrilla=document.querySelector(".grilla__Procuto");
-const nuevaGrilla=  
-       `<div class="grilla__Procuto">
-            <div class="img__paraGrilla" ><img class="imgGrilla" src="${img}"></div>
-            <div class="grilla__descripcion">  Descripcion: <p class="texto__grilla-descripcion"> 
-             ${descripcion}</p></div>
-            <h2 class="grilla__precio">Precio ${precio}</h2>
-            <button class="grilla__btnComprar">COMPRAR</button>
-        </div> `;
-montajeGrilla.insertAdjacentHTML('afterend',nuevaGrilla);
-console.log(crearGrilla) 
-}
-articuloNuevo()
+    }
+  }
+};
+
+articuloNuevo();
+
